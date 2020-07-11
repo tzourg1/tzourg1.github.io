69d3092feca1b99be3711eee3c7b265f6cfd4459
Tseren Zurganov <tserenzurganov@gmail.com>
2020-07-11 15:14:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ const myfirstelement =     <div>
           <li class="nav-item">
             <a class="nav-link" href="/articles">Articles</a>
           </li>
+    <li class="nav-item">
+            <a class="nav-link" href="https://paradoxbjj.freeforums.net/">Forums</a>
+          </li>
           <li class="nav-item">
             <a class="nav-link" href="/contact">Contact</a>
           </li>
