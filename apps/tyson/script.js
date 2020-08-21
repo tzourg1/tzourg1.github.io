6dc046aa6b70eb4c9d06f01036db6e4b23f58c9d
Tseren Zurganov <tserenzurganov@gmail.com>
2020-08-20 23:08:23 -0400

    Update script.js

--- apps/tyson/script.js
+++ apps/tyson/script.js
@@ -22,5 +22,11 @@ var newQuote = function(){
 "Im a convicted rapist, I'm a good father, I'm a semi-good husband.",
 "I paid a worker at New York's zoo to re-open it just for me and Robin. When we got to the gorilla cage there was 1 big silverback gorilla there just bullying all the other gorillas. They were so powerful but their eyes were like an innocent infant. I offered the attendant $10,000 to open the cage and let me smash that silverback's snotbox! He declined.",
 "I could sell out madison square gardens masturbating",
-"I'm gonna f you till you love me."                     
+"I'm gonna f you till you love me.",
+"God lets everything happen for a reason. It's all a learning process, and you have to go from one level to another.",
+"I want to rip out his heart and feed it to Lennox Lewis. I want to kill people. I want to rip their stomachs out and eat their children.",
+"When I was in prison, I was wrapped up in all those deep books. That Tolstoy crap - people shouldn't read that stuff.",
+"I think the average person thinks I'm a nut and I deserve whatever happens to me.",
+"I'm the biggest fighter in the history of the sport. If you don't believe it, check the cash register."
+"People are going to say what they say. I know sometimes I say things; I offend people."
 ]
