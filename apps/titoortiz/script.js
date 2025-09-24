f58d939f59316a15d76c6c6f60e4eca95808b164
Tseren Zurganov <tserenzurganov@gmail.com>
2025-09-24 01:45:53 -0400

    Update script.js

--- apps/titoortiz/script.js
+++ apps/titoortiz/script.js
@@ -15,18 +15,11 @@ var newQuote = function(){
 };
 
                                   var quotes = [
-"Everyone has a plan 'till they get punched in the mouth.",
-"As long as we persevere and endure, we can get anything we want.",
-"I normally don't do interviews with women unless I fornicate with them. So you shouldnt talk anymore unless you wanna...you know",
-"My back is broken...[Reporter: What Part?]... Spinal",
-"Im a convicted rapist, I'm a good father, I'm a semi-good husband.",
-"I paid a worker at New York's zoo to re-open it just for me and Robin. When we got to the gorilla cage there was 1 big silverback gorilla there just bullying all the other gorillas. They were so powerful but their eyes were like an innocent infant. I offered the attendant $10,000 to open the cage and let me smash that silverback's snotbox! He declined.",
-"I could sell out madison square gardens masturbating",
-"I'm gonna f you till you love me.",
-"God lets everything happen for a reason. It's all a learning process, and you have to go from one level to another.",
-"I want to rip out his heart and feed it to Lennox Lewis. I want to kill people. I want to rip their stomachs out and eat their children.",
-"When I was in prison, I was wrapped up in all those deep books. That Tolstoy crap - people shouldn't read that stuff.",
-"I think the average person thinks I'm a nut and I deserve whatever happens to me.",
-"I'm the biggest fighter in the history of the sport. If you don't believe it, check the cash register.",
-"People are going to say what they say. I know sometimes I say things; I offend people."
+"Jealous of him? This guy can't even put a f*ckin' sentence together man, are you kidding me right now? He's reaching for - he's reaching for those grapes. He's trying to make his wine, and the wine's already soundin' like a violin with that cheese and wine...um...we'll see on November 24th.",
+"I wanna outlive my children, of course, 100%.",
+"United we stand, divided, we will not fall",
+"I train six days, actually six days a week. Five days a week, I’ll train three days a week. One of those days I will train two days of the week. So, six days a week I will be training.",
+"Let me tell you how you feeling right now.",
+"Well here we are with a br.. S'raldo Babalu you did an awesome job so what you're a blackbelt in juijitsu gettin' a awesome submission there. I wanna tell me, what you see, lets go ahead and see it by the fight, what you saw, in the ring",
+"I'm only 43 years old. I'm an old mature, I mean I matured old, I mean... very fast, but at the same time my body hasn't matured great just because of... my father's really old my father is 80 years old, my grandmother's 100 years old, so my body is in great shape."
 ]
